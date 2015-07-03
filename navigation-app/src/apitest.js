5153e60fce34ad6324c540628e8a71c09780c731
Vladimir Totocevski <sephirothmk@gmail.com>
2015-07-03 16:06:53 +0200

    More testing

--- navigation-app/src/apitest.js
+++ navigation-app/src/apitest.js
@@ -4,20 +4,21 @@ import { HttpClient } from 'aurelia-http-client';
 @inject(HttpClient)
 export class API {
   heading = 'TestAPI';
-  posts = [];
+  posts = new Object;
   posttest = "staticen test";
+  glupost;
   url = 'http://localhost:62416/api/values';
 
   constructor(http) {
     this.http = http;
   }
 
-  
+
   activate() {
    var settings = {
   "async": true,
   "crossDomain": true,
-  "url": "http://localhost:62416/api/values",
+  "url": "http://localhost:62416/api/languages/1/resources/1",
   "method": "GET",
   "headers": {}
   };
@@ -25,10 +26,14 @@ export class API {
   $.ajax(settings).done(function (response) {
    this.posts = response;
    console.log(this.posts);
-   return this.posts;
+   this.glupost = this.posts.resourceName;
+   console.log(this.glupost);
+   return this.glupost;
    });
   }
-
+  sayHello(){
+    alert(API.glupost);
+  }
   canDeactivate() {
     return confirm('Are you sure you want to leave?');
   }
